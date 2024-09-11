7e4c251deed9e191fde5e30352e8f16b0cbc1c2a
Martin Bebey <37882687+martinbebey@users.noreply.github.com>
2024-09-11 19:41:18 -0400

    added fab

--- app/src/main/java/com/developer/wishlistapp/HomeView.kt
+++ app/src/main/java/com/developer/wishlistapp/HomeView.kt
@@ -1,13 +1,20 @@
 package com.developer.wishlistapp
 
+import android.graphics.drawable.Icon
 import android.widget.Toast
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeView(){
@@ -16,10 +23,24 @@ fun HomeView(){
     Scaffold(
         topBar = { AppBarView(title = "WishList", {
             Toast.makeText(context, "Back button clicked", Toast.LENGTH_LONG).show()
-        })}
+        })},
+        floatingActionButton = {
+            FloatingActionButton(
+                modifier = Modifier.padding(all = 20.dp),
+                contentColor = Color.White,
+                backgroundColor = Color.Black,
+                onClick = {
+                    Toast.makeText(context, "FAB button clicked", Toast.LENGTH_LONG).show()
+                }
+            ){
+                Icon(imageVector = Icons.Default.Add, contentDescription = null)
+            }
+        }
     ) {
         LazyColumn(
-            modifier = Modifier.fillMaxSize().padding(it)
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
         ) {
 
         }
