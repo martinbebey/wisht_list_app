ba65c054c0dba2e1a3cdee691f3bc22e0c7631e4
Martin Bebey <37882687+martinbebey@users.noreply.github.com>
2024-09-11 19:22:47 -0400

    added top bar navigation button

--- app/src/main/java/com/developer/wishlistapp/AppBar.kt
+++ app/src/main/java/com/developer/wishlistapp/AppBar.kt
@@ -3,9 +3,14 @@ package com.developer.wishlistapp
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 
@@ -13,8 +18,20 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun AppBarView(
     title: String,
-    onBackNavClicked: () -> Unit = {}
+    onBackNavClicked: @Composable () -> Unit = {}
 ){
+
+    val navigationIcon: (@Composable () -> Unit)? = {
+
+        IconButton(onClick = {onBackNavClicked()}){
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                tint = Color.White,
+                contentDescription = null
+            )
+        }
+    }
+
     TopAppBar(
         title = {
             Text(
@@ -27,6 +44,7 @@ fun AppBarView(
         },
         
         elevation = 3.dp,
-        backgroundColor = colorResource(id = R.color.app_bar_color)
+        backgroundColor = colorResource(id = R.color.app_bar_color),
+        navigationIcon = navigationIcon
     )
 }
--- app/src/main/java/com/developer/wishlistapp/HomeView.kt
+++ app/src/main/java/com/developer/wishlistapp/HomeView.kt
@@ -1,17 +1,22 @@
 package com.developer.wishlistapp
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 
 @Composable
 fun HomeView(){
-    
+    val context = LocalContext.current
+
     Scaffold(
-        topBar = { AppBarView(title = "WishList")}
+        topBar = { AppBarView(title = "WishList", {
+            Toast.makeText(context, "Back button clicked", Toast.LENGTH_LONG).show()
+        })}
     ) {
         LazyColumn(
             modifier = Modifier.fillMaxSize().padding(it)
