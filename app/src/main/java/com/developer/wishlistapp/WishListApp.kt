5d61a79ea4cef3dd642fae8557dcbe46af6b5b07
Martin Bebey <37882687+martinbebey@users.noreply.github.com>
2024-09-12 18:56:07 -0400

    set up app context and manifest for db

---
+++ app/src/main/java/com/developer/wishlistapp/WishListApp.kt
@@ -0,0 +1,11 @@
+package com.developer.wishlistapp
+
+import android.app.Application
+
+class WishListApp:Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Graph.provide(this)
+    }
+}
